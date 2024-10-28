642cff2d567205859e313652a4d3b5368e5f0e03
shalu2001 <danthanarayanas.21@uom.lk>
2024-10-28 19:29:36 +0530

    Add react-auth-kit dependency and configure AuthStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
+import AuthProvider from 'react-auth-kit'
+import store from './AuthStore'
 
 function App() {
 
   return (
-    <>
+    <AuthProvider store={store}>
       <div className='text-5xl text-yellow-300'>
         hello world
       </div>
-    </>
+    </AuthProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import AuthProvider  from 'react-auth-kit'
+import store from './AuthStore.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider store={store}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
