cc3908dab6924358cc52913936712d5525ec1183
shalu2001 <danthanarayanas.21@uom.lk>
2025-02-28 14:56:53 +0530

    Update axios base URL to localhost:3001 for API calls

--- src/actions/axiosInstance.ts
+++ src/actions/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000"; // Use env variable here
+const baseURL = "http://localhost:3001"; // Use env variable here
 
 const axiosInstance = axios.create({
   baseURL,
